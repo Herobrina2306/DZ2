046c8b33875a7be30151baf16447eb6f877c6094
AleksandraM <musatova.aleksandra@mail.ru>
2022-09-03 13:57:21 +0300

    задача 15 c секретом. введите число больше или меньше

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+string[] array = {"от одного до семи", "нет", "нет", "нет", "нет", "нет", "да", "да"};
+Console.WriteLine("Введите число от одного до семи");
+int i = int.Parse(Console.ReadLine());
+if(i < 1 || i > array.Length)
+{
+    Console. WriteLine("От одного до семи. Попробуйте ещё раз.");
+}
+else
+{
+    Console.WriteLine($"{i} -> {array[i]}");
+} 
+
+
+Console.WriteLine("Введите число от одного до семи");
+int a = int.Parse(Console.ReadLine());
+if(a < 1 || a > 8)
+{
+    Console. WriteLine("От одного до семи.");
+}
+else
+{
+    if(a == 6 || a ==7)
+    {
+        Console.WriteLine($"{a} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{a} -> нет");
+    }
+}
